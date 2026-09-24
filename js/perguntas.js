2e2e7e0991529956d5ff7f905b3f651ef20914cb
Laraliziany <rickli.lara@escola.pr.gov.br>
2026-09-24 11:35:01 -0300

    kkj

--- js/perguntas.js
+++ js/perguntas.js
@@ -4,15 +4,13 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "A diminuição dos animais e o risco de extinção.",
-                afirmacao: [
-                    ["Você se preocupa principalmente com a vida e a sobrevivência dos animais.",
+                afirmacao: ["Você se preocupa principalmente com a vida e a sobrevivência dos animais.",
                     "Você se preocupa com o equilíbrio do ecossistema e entende que cada espécie extinta desregula toda a cadeia da natureza.",
                 ]
             },
         
                 texto:  "A destruição dos ambientes naturais e dos ecossistemas.",
-                afirmacao: [
-                    ["Você valoriza o equilíbrio da natureza e a preservação dos habitats.",
+                afirmacao:["Você valoriza o equilíbrio da natureza e a preservação dos habitats.",
                     "Você acredita que pequenas atitudes responsáveis de cada indivíduo somam forças e criam uma corrente poderosa para combater práticas que destroem o meio ambiente.",
         
                     ]
@@ -26,16 +24,14 @@ export const perguntas = [
             alternativas: [
                 {
                     texto: "Denunciaria atividades ilegais às autoridades responsáveis...",
-                    afirmacao: [
-                    ["Você acredita que atitudes responsáveis ajudam a combater práticas que prejudicam a natureza.",
+                    afirmacao: ["Você acredita que atitudes responsáveis ajudam a combater práticas que prejudicam a natureza.",
                         " Você acredita que pequenas atitudes responsáveis de cada indivíduo somam forças e criam uma corrente poderosa para combater práticas que destroem o meio ambiente.",
                     ]
 
                 },
                 
                     texto:  "Conversaria com outras pessoas para conscientizá-las sobre o problema.. ",
-                    afirmacao: [
-                        ["amigo da Sustentabilidade — Você procura maneiras de aproveitar os recursos naturais sem prejudicar o meio ambiente.",
+                    afirmacao:["amigo da Sustentabilidade — Você procura maneiras de aproveitar os recursos naturais sem prejudicar o meio ambiente.",
                          "Você acredita que atitudes responsáveis no dia a dia, como recusar produtos de origem duvidosa, ajudam a sufocar o comércio ilegal que prejudica a natureza.",
                     ]
                 }    
@@ -48,15 +44,13 @@ export const perguntas = [
             alternativas: [
                 {
                     texto: "Respeitar as leis que protegem a fauna e os períodos de reprodução..",
-                    afirmacao: [
-                    ["Você acredita que seguir as regras é fundamental para preservar os animais..",
+                    afirmacao: ["Você acredita que seguir as regras é fundamental para preservar os animais..",
                     " Você acredita que a mudança começa em casa com hábitos sustentáveis, mostrando que a responsabilidade diária é a chave para proteger a biodiversidade."
                     ]
                 },
                 {
                     texto:    "incentivar práticas de pesca e atividades na natureza de forma sustentável..",
-                    afirmacao: [
-                    ["É fundamental incentivar práticas de pesca e atividades na natureza de forma sustentável, garantindo a preservação dos ecossistemas, a proteção das espécies e o uso responsável dos recursos naturais para as futuras gerações..",
+                    afirmacao: ["É fundamental incentivar práticas de pesca e atividades na natureza de forma sustentável, garantindo a preservação dos ecossistemas, a proteção das espécies e o uso responsável dos recursos naturais para as futuras gerações..",
                     "Você acredita que dar o exemplo com atitudes responsáveis inspira outras pessoas a também combaterem as ações que prejudicam a fauna e a flora."
                     ]
                 }    
